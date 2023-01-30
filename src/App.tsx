4332317ace5ea0c548184a1dd03fd3af3fa216da
jinbaek <jinmb0602@gmail.com>
2023-01-30 11:29:36 +0900

    set up the router

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
 import { ReactNode } from 'react';
+import { Outlet } from 'react-router-dom';
+import Header from './components/Header';
 
 type AppProps = {};
 const App = ({}: AppProps) => {
-  return <div>App</div>;
+  return (
+    <div>
+      <Header />
+      <Outlet />
+    </div>
+  );
 };
 
 export default App;
---
+++ src/components/Header.tsx
@@ -0,0 +1,8 @@
+import { ReactNode } from 'react';
+
+type HeaderProps = {};
+const Header = ({}: HeaderProps) => {
+  return <div>Header</div>;
+};
+
+export default Header;
